45729d6d695c7e032cdd3a1e97e36d0dd21d1dd4
Alexander Rogers <alexander.caelan.rogers@gmail.com>
2021-07-16 14:29:52 -0400

    add AJAX functions

--- public/JS/main.js
+++ public/JS/main.js
@@ -16,3 +16,33 @@ const $venueDivs = [$("#venue1"), $("#venue2"), $("#venue3"), $("#venue4")];
 const $weatherDiv = $("#weather1");
 const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
+// AJAX functions here:
+const getVenues = async () => {
+    const city = $input.val();
+    const urlToFetch = `${url}${city}&limit=10&client_id=${clientId}&client_secret=${clientSecret}&v=20180101`;
+    try {
+      const response = await fetch(urlToFetch);
+      if (response.ok) {
+        const jsonResponse = await response.json();
+        const venues = jsonResponse.response.groups[0].items.map(item => item.venue);
+        console.log(venues);
+        return venues;
+      }
+    } catch(error){
+      console.log(error);
+    }
+}
+
+const getForecast = async () => {
+    const urlToFetch = `${weatherUrl}?&q=${$input.val()}&APPID=${openWeatherKey}`;
+    try {
+      const response = await fetch(urlToFetch);
+      if (response.ok) {
+        const jsonResponse = await response.json();
+        return jsonResponse;
+      }
+    } catch(error) {
+      console.log(error);
+    }
+}  
+
