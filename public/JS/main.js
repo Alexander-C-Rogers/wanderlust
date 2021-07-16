40600d2f89d6429cabad4c8fa23e2a0d9266f8e8
Alexander Rogers <alexander.caelan.rogers@gmail.com>
2021-07-16 14:27:49 -0400

    add page elements to main.js

--- public/JS/main.js
+++ public/JS/main.js
@@ -7,3 +7,12 @@ const url = 'https://api.foursquare.com/v2/venues/explore?near=';
 const openWeatherKey = '838c7a693b58a464bbeeee4603300656';
 const weatherUrl = 'https://api.openweathermap.org/data/2.5/weather';
 
+// Page Elements
+const $input = $('#city');
+const $submit = $('#button');
+const $destination = $('#destination');
+const $container = $('.container');
+const $venueDivs = [$("#venue1"), $("#venue2"), $("#venue3"), $("#venue4")];
+const $weatherDiv = $("#weather1");
+const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
