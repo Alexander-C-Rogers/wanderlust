be38df9d9a723fe47be55c8222d36bc6ee7caa83
Alexander Rogers <alexander.caelan.rogers@gmail.com>
2021-07-16 14:26:47 -0400

    create main.js and link to index.html

---
+++ public/JS/main.js
@@ -0,0 +1,9 @@
+// Foursquare API Info
+const clientId = 'DSJUZOOV20KM0VBXLO2GUNLOA1PYX0T3UVJW3EINW0DUTKOI';
+const clientSecret = 'CJUVGKKS4UXANC4PUSBXSOOWHW4YVLHGQ0AQQ3TYERA5OUCX';
+const url = 'https://api.foursquare.com/v2/venues/explore?near=';
+
+// OpenWeather Info
+const openWeatherKey = '838c7a693b58a464bbeeee4603300656';
+const weatherUrl = 'https://api.openweathermap.org/data/2.5/weather';
+
